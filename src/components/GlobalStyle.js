8ab6e116017d19b574163b29156bb30a438396e1
sinafischer <sina.h.fischer@gmail.com>
2020-07-03 16:37:14 +0200

    Finish project set-up

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+:root {
+  --primary: #bae8e8;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-size: 18px;
+}
+`
