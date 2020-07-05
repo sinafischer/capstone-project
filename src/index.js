b5028a3ec9a21e92f54029d89ffdda14e76320ca
sinafischer <sina.h.fischer@gmail.com>
2020-07-05 14:44:21 +0200

    Adjust set-up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import GlobalStyle from './components/GlobalStyle'
+import GlobalStyle from './GlobalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
