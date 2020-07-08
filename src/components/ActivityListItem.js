ff5cd3bfbed2a4398e0372bc7df9edf59d084508
sinafischer <sina.h.fischer@gmail.com>
2020-07-08 21:13:07 +0200

    Add show-more-text

--- src/components/ActivityListItem.js
+++ src/components/ActivityListItem.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import activities from '../activities.json'
 import styled from 'styled-components'
+import ShowMoreText from 'react-show-more-text'
 
 export default function ActivityList() {
   return (
     <>
       {activities.map((activity) => {
         return (
-          <li>
+          <li key={activity.name}>
             <DivisionLine />
             <h3>{activity.name}</h3>
-            <p>{activity.details}</p>
+            <ShowMoreText lines={1} more="More" less="Less" expanded={false}>
+              <p>{activity.details}</p>
+            </ShowMoreText>
           </li>
         )
       })}
