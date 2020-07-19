79eb3ce50a9815f625f8d396eec0468b5a0b438e
sinafischer <sina.h.fischer@gmail.com>
2020-07-19 17:39:19 +0200

    Adjust storybook for form

--- src/components/AddActivityList.stories.js
+++ src/components/AddActivityList.stories.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import AddActivityForm from './AddActivityForm'
-import GlobalStyle from '../GlobalStyle'
-import { addDecorator } from '@storybook/react'
 
 export default {
   title: 'AddActivityForm',
